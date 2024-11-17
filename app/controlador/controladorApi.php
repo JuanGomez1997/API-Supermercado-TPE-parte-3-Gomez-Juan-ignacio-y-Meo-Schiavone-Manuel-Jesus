09d0c846f19b4c41088e1799941670e777d0783c
unknown <chijuan.1997@gmail.com>
2024-11-17 05:04:33 -0300

    Creando archivos

---
+++ app/controlador/controladorApi.php
@@ -0,0 +1,17 @@
+<?php
+    require_once './app/vista/vistaAPI.php';
+    
+    abstract class ControladorApi {
+        protected $vista;
+        private $dato;
+        
+        function __construct() {
+            $this->vista = new VistaApi();
+            $this->dato = file_get_contents('php://input');
+        }
+
+        function obtenerDatos() {
+            return json_decode($this->dato);
+        }
+    }
+?>
---
+++ app/vista/vistaAPI.php
@@ -0,0 +1,25 @@
+<?php
+    class VistaApi {
+        public function respuesta ($datos, $estado = 200) {
+            header('Content-type: application/json');
+            header('HTTP/1.1 ' . $estado . " " . $this->_estadoSolicitud($estado));
+            echo json_encode($datos);
+
+        }
+
+        private function _estadoSolicitud($codigo) {
+            $estado = array(
+                200 => "OK",
+                201 => "Creado",
+                400 => "Solicitud Incorrecta",
+                404 => "No Encontrado",
+                500 => "Error Interno del Servidor",
+            );
+            if(!isset($estado[$codigo])) {
+                $codigo = 500;
+            }
+            return $estado[$codigo];
+
+        }
+
+    }
